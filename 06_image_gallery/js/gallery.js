07b68f303da68fb00581159eac80d65eda61bc33
sharryhong <kshopzoa15@gmail.com>
2017-01-04 22:41:21 +0900

    IE에서 for of문 안되는 것 해결

--- 06_image_gallery/js/gallery.js
+++ 06_image_gallery/js/gallery.js
@@ -13,21 +13,33 @@
 			var data = JSON.parse(this.response);
 			var template = '';
 			var photos = data.results;
-			for(var photo of photos){
+			for(var i=0; i<photos.length; i++){
 				template += [
 					'<li>',
 						'<a href="#" class="photo-link">',
-							'<img class="photo-img" src="'+photo.image+'" alt="'+photo.alt+'">',
+							'<img class="photo-img" src="'+photos[i].image+'" alt="'+photos[i].alt+'">',
 						'</a>',
 					'</li>'
 				].join('');
 			}
+			// IE에서는 for of 문이 안됨 
+			// for(var photo of photos){
+			// 	template += [
+			// 		'<li>',
+			// 			'<a href="#" class="photo-link">',
+			// 				'<img class="photo-img" src="'+photo.image+'" alt="'+photo.alt+'">',
+			// 			'</a>',
+			// 		'</li>'
+			// 	].join('');
+			// }
 		} else {
 			console.log('통신 데이터 전송 실패');
 		}
 		result_view.innerHTML = template;
+		console.log('okok')
 	}
 
 	xhr.send();
+
 })(this, this.XMLHttpRequest || this.ActiveXObject('Microsoft.XMLHTTP'));
 
