1f3097e787caf2038ae2464a2dcb314523c4925d
sharryhong <kshopzoa15@gmail.com>
2017-01-04 16:39:22 +0900

    ajax, json불러오기

--- 06_image_gallery/js/gallery.js
+++ 06_image_gallery/js/gallery.js
@@ -2,12 +2,29 @@
 	'use strict';
 
 	var xhr = new XHR;
+	var result_view = document.querySelector('.ajax-result');
+
 	xhr.open("GET", "./data/gallery.json");
 	xhr.send();
+
 	xhr.onreadystatechange = function() {
-		if( this.status === 200 && this.readyState === 4 ) {
-			console.log(this.response);
+		if(this.status === 200 && this.readyState === 4) {
+			console.log('통신 데이터 전송 성공! ^^'); 
+			// console.log(this.response);
+			var data = JSON.parse(this.response);
+			var template = '';
+			var photos = data.results;
+			for(var photo of photos){
+				template += [
+					'<li>',
+						'<img src="'+photo.image+'" alt="">',
+					'</li>'
+				].join('');
+			}
+		} else {
+			console.log('통신 데이터 전송 실패');
 		}
+		result_view.innerHTML = template;
 	}
 
 })(this, this.XMLHttpRequest || this.ActiveXObject('Microsoft.XMLHTTP'));
