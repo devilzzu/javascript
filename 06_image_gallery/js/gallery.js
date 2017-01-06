9da5cfba10c310625fa2cda72de1db4577f25de8
sharryhong <kshopzoa15@gmail.com>
2017-01-06 22:41:42 +0900

    클릭시 확대.. 어떻게 해야할까 고민중

--- 06_image_gallery/js/gallery.js
+++ 06_image_gallery/js/gallery.js
@@ -5,7 +5,7 @@
 	var result_view = document.querySelector('.ajax-result');
 
 	xhr.open("GET", "./data/gallery.json");
-
+	xhr.send();
 	xhr.onreadystatechange = function() {
 		if(this.status === 200 && this.readyState === 4) {
 			console.log('통신 데이터 전송 성공! ^^'); 
@@ -14,6 +14,19 @@
 			var template = '';
 			var photos = data.results;
 			for(var i=0; i<photos.length; i++){
+				/* 다른방법 
+				var li = document.createElement("li");
+				var a = document.createElement("a");
+				var img = document.createElement("img");
+				a.setAttribute("class", "photo-link");
+				a.setAttribute("href", "javascript:;");
+				img.setAttribute("class", "photo-img");
+				img.setAttribute("src", photos[i].image);
+				li.appendChild(a);
+				a.appendChild(img);
+				console.log(li)
+				result_view.appendChild(li); 
+				*/
 				template += [
 					'<li>',
 						'<a href="javascript:;" class="photo-link">',
@@ -22,24 +35,41 @@
 					'</li>'
 				].join('');
 			}
-			// IE에서는 for of 문이 안됨 
-			// for(var photo of photos){
-			// 	template += [
-			// 		'<li>',
-			// 			'<a href="#" class="photo-link">',
-			// 				'<img class="photo-img" src="'+photo.image+'" alt="'+photo.alt+'">',
-			// 			'</a>',
-			// 		'</li>'
-			// 	].join('');
-			// }
+
 		} else {
 			console.log('통신 데이터 전송 실패');
 		}
 		result_view.innerHTML = template;
 
+		var photoLink = document.querySelectorAll('.photo-link');
+		var photoImg = document.querySelectorAll('.photo-img');
+		var headerHeight = document.querySelector('.page-header').offsetHeight + 10;
+		var windowWidth = window.innerWidth;
+		var windowHeight = window.innerHeight;
+		var chk = true;
+		for(var i=0; i<photoLink.length; i++){
+			var photoLinkWidth = photoLink[i].offsetWidth;
+			console.log(photoLinkWidth);
+			photoLink[i].onclick = function() {
+				
+				if(chk){
+					// this.style.transform = 'scale(2)';
+					this.classList.add("on");
+					// this.firstElementChild.style.height = (windowHeight-headerHeight)+'px';
+					chk = false;
+				}else{
+					// this.style.transform = 'scale(1)';
+					this.classList.remove("on");
+					// this.style.width = photoLinkWidth+'px';
+					chk = true;
+				}
+
+			}
+		}
+		
 	}
 
-	xhr.send();
+	
 
 })(this, this.XMLHttpRequest || this.ActiveXObject('Microsoft.XMLHTTP'));
 
