b6108a15822041a09f7b35fe1e75fc1af2a97337
sharryhong <kshopzoa15@gmail.com>
2017-02-08 17:14:41 +0900

    es6 class + arrow function

--- es6/arrow-es6.js
+++ es6/arrow-es6.js
@@ -1,11 +1,11 @@
 // ES6
-// Person class. 생성자함수
+// Person class
 class Person {
-	
+	// 생성자함수
 	constructor(name){
 		this.name = name;
 	}
-
+	// 메소드
 	myFrineds(friends) {
 		let arr = friends.map( el => `${el} is friends with ${this.name}` );
 		console.log(arr);
